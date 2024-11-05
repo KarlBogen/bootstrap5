448a504f52f23e6234ddcaeedf6af953ed4d8a99
karlk <karl-klostermann@t-online.de>
2024-11-05 09:38:11 +0100

    Release 1.1.7

--- new_files/templates/bootstrap5/javascript/extra/default.js.php
+++ new_files/templates/bootstrap5/javascript/extra/default.js.php
@@ -90,61 +90,78 @@
 	});
 <?php }
 // Beginn Autocomplete
-if (SEARCH_AC_STATUS == 'true') { ?>
+if (SEARCH_AC_STATUS == 'true' || (basename($PHP_SELF) != FILENAME_SHOPPING_CART && !strpos($PHP_SELF, 'checkout'))) { ?>
+	function ac_closing() {
+		$('#inputStringSubmit').prop("disabled",false);
+		setTimeout("$('#suggestions').slideUp();", 100);
+	}
+<?php } ?>
+<?php if (SEARCH_AC_STATUS == 'true') { ?>
   var session_id = '<?php echo xtc_session_id(); ?>';
-  $('body').on('keyup paste cut input focus', '#inputString', delay(function() {
-    if ($(this).length == 0) {
-      $('#suggestions').hide();
-    } else {
-      var post_params = $('#quick_find').serialize();
-      $.ajax({
-        dataType: "json",
-        type: 'post',
-        url: '<?php echo DIR_WS_BASE; ?>ajax.php?ext=get_autocomplete&MODsid='+session_id,
-        data: post_params,
-        cache: false,
-        async: true,
-        success: function(data) {
-          if (data !== null && typeof data === 'object') {
-            if (data.result !== null && data.result != undefined && data.result != '') {
-              $('#autoSuggestionsList').html(decode_ajax(data.result));
-              $('#suggestions').slideDown();
-              $('#inputStringSubmit').prop("disabled",true);
-            } else {
-              $('#suggestions').slideUp();
-              $('#inputStringSubmit').prop("disabled",false);
-            }
+
+  function ac_ajax_call(post_params) {
+    $.ajax({
+      dataType: "json",
+      type: 'post',
+      url: '<?php echo DIR_WS_BASE; ?>ajax.php?ext=get_autocomplete&MODsid='+session_id,
+      data: post_params,
+      cache: false,
+      async: true,
+      success: function(data) {
+        if (data !== null && typeof data === 'object') {
+          if (data.result !== null && data.result != undefined && data.result != '') {
+            $('#autoSuggestionsList').html(ac_decode(data.result));
+            $('#suggestions').slideDown();
+            $('#inputStringSubmit').prop("disabled",true);
+          } else {
+            $('#suggestions').slideUp();
+            $('#inputStringSubmit').prop("disabled",false);
           }
         }
-      });
-    }
-  }, 500));
-  function delay(fn, ms) {
+      }
+    });
+  }
+
+  function ac_delay(fn, ms) {
     let timer = 0;
     return function(args) {
       clearTimeout(timer);
       timer = setTimeout(fn.bind(this, args), ms || 0);
     }
   }
-  function decode_ajax(encodedString) {
+
+  function ac_decode(encodedString) {
     var textArea = document.createElement('textarea');
     textArea.innerHTML = encodedString;
+
     return textArea.value;
   }
+
+  $('body').on('keydown paste cut input focus', '#inputString', ac_delay(function() {
+    if ($(this).length == 0) {
+      $('#suggestions').hide();
+    } else {
+      let post_params = $('#quick_find').serialize();
+      ac_ajax_call(post_params);
+    }
+  }, 500));
+
   $('body').on('click', function (e) {
-    if ($(e.target).closest("#suggestions").length === 0) {
+    if ($(e.target).closest("#suggestions").length === 0
+        && $(e.target).closest("#quick_find").length === 0
+        )
+    {
       ac_closing();
     }
   });
-<?php
-}
-if (SEARCH_AC_STATUS == 'true' || (basename($PHP_SELF) != FILENAME_SHOPPING_CART && strpos($PHP_SELF, 'checkout') === false)) { ?>
-	function ac_closing() {
-		$('#inputStringSubmit').prop("disabled",false);
-		setTimeout("$('#suggestions').slideUp();", 100);
-	}
-<?php
-}
+
+<?php if(defined('SEARCH_AC_CATEGORIES') && SEARCH_AC_CATEGORIES == 'true') { ?>
+  $('body').on('change', '#cat_search', ac_delay(function() {
+    let post_params = $('#quick_find').serialize();
+    ac_ajax_call(post_params);
+  }, 500));
+<?php } ?>
+<?php }
 // Ende Autocomplete
 // Beginn Alert
 ?>
--- new_files/templates/bootstrap5/source/boxes/login.php
+++ new_files/templates/bootstrap5/source/boxes/login.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: login.php 15291 2023-07-06 11:46:25Z GTB $
+   $Id: login.php 16178 2024-10-15 12:23:58Z Markus $
 
    modified eCommerce Shopsoftware
    http://www.modified-shop.org
@@ -26,7 +26,6 @@
     $box_smarty->assign('LINK_CREATE_GUEST_ACCOUNT', xtc_href_link(FILENAME_CREATE_GUEST_ACCOUNT, '', 'SSL'));
     $box_smarty->assign('FORM_END', '</form>');
   } else {
-    $box_smarty->assign('IS_CUSTOMER', ($_SESSION['customers_status']['customers_status_id'] != DEFAULT_CUSTOMERS_STATUS_ID_GUEST) ? 1 : 0);
     $box_smarty->assign('LINK_ACCOUNT', xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
     $box_smarty->assign('LINK_EDIT', xtc_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'));
     $box_smarty->assign('LINK_ADDRESS', xtc_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'));
