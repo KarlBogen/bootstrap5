11b4eb8fe5ead0d4cc01baf0c26e1ed99c70f767
karlk <karl-klostermann@t-online.de>
2025-01-08 11:37:44 +0100

    cookieconsent.js.php utf-8

--- new_files/templates/bootstrap5/javascript/extra/cookieconsent.js.php
+++ new_files/templates/bootstrap5/javascript/extra/cookieconsent.js.php
@@ -104,7 +104,7 @@ if (defined('MODULE_COOKIE_CONSENT_STATUS') && strtolower(MODULE_COOKIE_CONSENT_
         __cmp('getVendorConsents', [], oilGtagCookie);
       }
       <?php } ?>
-      // Focus für Screenreader setzen
+      // Focus fuer Screenreader setzen
       if (event && event.data && (eventDataContains('oil_optin_done'))) {
 			 setTimeout(function() {$("#focus").focus()}, 300);
       }
