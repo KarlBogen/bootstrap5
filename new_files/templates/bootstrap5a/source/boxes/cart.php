fc6b0b82c53cca748e62546c66cbd1a31c0e3989
karlk <karl-klostermann@t-online.de>
2025-02-26 20:10:56 +0100

    Update box cart for PayPal PLUS rev 16207

--- new_files/templates/bootstrap5a/source/boxes/cart.php
+++ new_files/templates/bootstrap5a/source/boxes/cart.php
@@ -7,7 +7,7 @@
 
    Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
 
   // include smarty
@@ -25,24 +25,24 @@
   if ($_SESSION['cart']->count_contents() > 0) {
     $total = $_SESSION['cart']->show_total();
 
-    // build array with cart content and count quantity  
+    // build array with cart content and count quantity
     if (strpos($PHP_SELF, FILENAME_LOGOFF) === false) {
       $products = $_SESSION['cart']->get_products();
       for ($i = 0, $n = count($products); $i < $n; $i++) {
         $del_button = '<a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('action', 'box', 'prd_id')).'action=remove_product&box=cart&prd_id=' . $products[$i]['id'], 'NONSSL') . '">' . xtc_image_button('cart_del.gif', IMAGE_BUTTON_DELETE) . '</a>';
         $del_link = '<a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('action', 'box', 'prd_id')).'action=remove_product&box=cart&prd_id=' . $products[$i]['id'], 'NONSSL') . '">' . IMAGE_BUTTON_DELETE . '</a>';
-      
+
         $image = '';
         if ($products[$i]['image'] != '') {
           $image = $product->productImage($products[$i]['image'],'thumbnail');
         }
-        
+
         $mark_stock = '';
         if (STOCK_CHECK == 'true') {
           $mark_stock = xtc_check_stock($products[$i]['id'], $products[$i]['quantity'], $products[$i]['stock']);
           if ($mark_stock) $any_out_of_stock = true;
         }
-  
+
         if (STOCK_CHECK_SPECIALS == 'true' && $xtPrice->xtcCheckSpecial($products[$i]['id'])) {
           $mark_stock = check_stock_specials($products[$i]['id'], $products[$i]['quantity']);
           if ($mark_stock) $any_out_of_stock = true;
@@ -50,7 +50,7 @@
 
 //        $qty += $products[$i]['quantity']; zählt falsch
         $qty = count($products);
-        
+
         $products_cart[] = array (
           'QTY' => $products[$i]['quantity'],
           'LINK' => xtc_href_link(FILENAME_PRODUCT_INFO, xtc_get_all_get_params_include(array('language')).'products_id='.$products[$i]['id']),
@@ -62,6 +62,22 @@
         );
       }
     }
+
+    if (defined('MODULE_PAYMENT_PAYPAL_SECRET')
+        && MODULE_PAYMENT_PAYPAL_SECRET != ''
+        )
+    {
+      // include needed classes
+      require_once(DIR_FS_EXTERNAL.'paypal/classes/PayPalPaymentV2.php');
+
+      $paypal = new PayPalPaymentV2('paypalexpress');
+      if ($paypal->is_enabled()) {
+        $box_smarty->assign('paypalexpress', true);
+        if ($paypal->get_config('MODULE_PAYMENT_'.strtoupper($paypal->code).'_SHOW_CART_BNPL') == '1') {
+         $box_smarty->assign('paypalbnpl', true);
+        }
+      }
+    }
   }
 
   if (STOCK_CHECK == 'true' && $any_out_of_stock === true) {
@@ -71,7 +87,7 @@
       $messageStack->add('box_cart', OUT_OF_STOCK_CANT_CHECKOUT);
     }
   }
-  
+
   if ($messageStack->size('box_cart') > 0) {
     $box_smarty->assign('error_message', $messageStack->output('box_cart'));
   }
@@ -97,14 +113,14 @@
 
   $box_smarty->assign('LINK_CART', xtc_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL'));
   $box_smarty->assign('LINK_CHECKOUT', xtc_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'));
-  
+
   if (defined('BS5_SHOW_BOX_ADD_QUICKIE') && BS5_SHOW_BOX_ADD_QUICKIE == 'true') {
     $box_smarty->assign('FORM_ACTION', xtc_draw_form('quick_add', xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array ('action')) . 'action=add_a_quickie', $request_type)));
     $box_smarty->assign('INPUT_FIELD', xtc_draw_input_field('quickie', '', 'id="input_quick_add" class="form-control"'));
     $box_smarty->assign('SUBMIT_BUTTON', xtc_image_submit('button_add_quick.gif', BOX_HEADING_ADD_PRODUCT_ID));
     $box_smarty->assign('FORM_END', '</form>');
   }
-  
+
   $box_smarty->caching = 0;
   $box_cart = $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_cart.html');
 
