98b081d9fa494a297c77689ac2c0aa1021967d05
karlk <karl-klostermann@t-online.de>
2025-03-29 14:35:03 +0100

    Release 1.1.10

--- new_files/includes/extra/modules/products_attributes_data/bs5_attribute_price_updater.php
+++ new_files/includes/extra/modules/products_attributes_data/bs5_attribute_price_updater.php
@@ -36,8 +36,8 @@ if (defined('BS5_MODULFUX_ATTRIBUTES_DEFAULT') && BS5_MODULFUX_ATTRIBUTES_DEFAUL
 	          'prefix'       => isset($products_options['price_prefix']) ? $products_options['price_prefix'] : '',
 	          'aprice'       => 0,
 	          'vpetext'      => encode_htmlentities($product->data['products_vpe'] != '0' ? xtc_get_vpe_name($product->data['products_vpe']) : BS5_TEXT_PRODUCTS_VPE),
-	          'vpevalue'     => (($product->data['products_vpe_status'] && (double)$product->data['products_vpe_value']) ? (double)$product->data['products_vpe_value'] : false),
-	          'attrvpevalue' => false,
+	          'vpevalue'     => (($product->data['products_vpe_status'] && (double)$product->data['products_vpe_value']) ? (double)$product->data['products_vpe_value'] : 0),
+	          'attrvpevalue' => 0,
 	          'onlytext'     => isset($json_onlytext) ? $json_onlytext : BS5_TXT_ONLY,
 	          'protext'      => isset($json_protext) ? $json_protext : TXT_PER,
 	          'insteadtext'  => isset($json_insteadtext) ? $json_insteadtext : BS5_TXT_INSTEAD,
@@ -49,6 +49,19 @@ if (defined('BS5_MODULFUX_ATTRIBUTES_DEFAULT') && BS5_MODULFUX_ATTRIBUTES_DEFAUL
 
 //BOF - web0null_attribute_price_updater
 if (defined('BS5_ATTR_PRICE_UPDATER') && BS5_ATTR_PRICE_UPDATER == 'true') {
+
+  // Attribut-VPE-Wert wird mit Hilfe des Gewichts-Prefix berechnet
+  // das JavaScript rechnet immer Artikel-VPE-Wert + Attribut-VPE-Wert
+  $attr_vpe_value = $products_options['attributes_vpe_value'];
+  switch ($products_options['weight_prefix']) {
+    case '-':
+      $attr_vpe_value = -1 * abs($products_options['attributes_vpe_value']);
+      break;
+    case '=':
+      $attr_vpe_value = $products_options['attributes_vpe_value'] - $product->data['products_vpe_value'];
+      break;
+  }
+
   $products_options_data[$row]['DATA'][$col]['JSON_ATTRDATA'] = str_replace(
     '"', '&quot;', json_encode(
       [
@@ -60,8 +73,8 @@ if (defined('BS5_ATTR_PRICE_UPDATER') && BS5_ATTR_PRICE_UPDATER == 'true') {
         'prefix'       => $products_options['price_prefix'],
         'aprice'       => $xtPrice->xtcFormat($price, false),
         'vpetext'      => encode_htmlentities($product->data['products_vpe'] != '0' ? xtc_get_vpe_name($product->data['products_vpe']) : BS5_TEXT_PRODUCTS_VPE),
-        'vpevalue'     => (($product->data['products_vpe_status'] && (double)$product->data['products_vpe_value']) ? (double)$product->data['products_vpe_value'] : false),
-        'attrvpevalue' => (($product->data['products_vpe_status'] && (double)$products_options['attributes_vpe_value']) ? (double)$products_options['attributes_vpe_value'] : false),
+        'vpevalue'     => (($product->data['products_vpe_status'] && (double)$product->data['products_vpe_value']) ? (double)$product->data['products_vpe_value'] : 0),
+        'attrvpevalue' => (($product->data['products_vpe_status'] && (double)$attr_vpe_value) ? (double)$attr_vpe_value : 0),
         'onlytext'     => isset($json_onlytext) ? $json_onlytext : BS5_TXT_ONLY,
         'protext'      => isset($json_protext) ? $json_protext : TXT_PER,
         'insteadtext'  => isset($json_insteadtext) ? $json_insteadtext : BS5_TXT_INSTEAD,
